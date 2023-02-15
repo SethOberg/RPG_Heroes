b7e275bad3bb7315425dc7cbfb4709e29293d438
SethOberg <seth.a.oberg@gmail.com>
2023-02-15 14:54:17 +0100

    Test: add failing test to see report results

--- src/test/java/TestMage.java
+++ src/test/java/TestMage.java
@@ -18,6 +18,12 @@ public class TestMage {
     }
 
     @Test
+    public void testFailing() {
+        var mage = new Mage("someName");
+        assertEquals(3, mage.getLevel());
+    }
+
+    @Test
     public void testMageBaseStats() {
         var mage = new Mage("someName");
         var mageBaseStats = mage.getLevelAttributes();
